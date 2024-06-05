65d4b9ba2bee414965d3852419b026baa035519a
Natasha Stander <natasha@scitools.com>
2024-06-05 15:49:44 -0600

    Revert "delete Shared Tasks project - Adding to Understand installation in scripts/python"
    This reverts commit 626113c2effa70bd2ec2bb6c38cb030edbba8b4a.

---
+++ Solutions/sharedTasks/README.md
@@ -0,0 +1,71 @@
+# Overview
+
+Visualize an architecture of functions with the Shared Tasks graph. See how tasks interact with global objects, and when interrupt control functions are involved.
+
+# Setup: Install the Graph Plugin
+
+1. [Download](https://raw.githubusercontent.com/stinb/plugins/main/Solutions/sharedTasks/sharedTasksGraph.upy) the Shared Tasks graph.
+
+2. Drag and drop it into Understand.
+
+# Setup: Install the CSV Interactive Report Plugin
+
+1. [Download](https://raw.githubusercontent.com/stinb/plugins/main/Solutions/sharedTasks/sharedTasksCSV.upy) the Shared Tasks CSV script.
+
+2. Drag and drop it into Understand.
+
+3. To have a dynamic file name, you can add {arch}, {date}, or {time} (case-insensitive) to the "CSV file name" option.
+
+# Setup: Use the CSV Script in the Command Line Interface
+
+1. [Download](https://raw.githubusercontent.com/stinb/plugins/main/Solutions/sharedTasks/sharedTasksCSV.upy) the Shared Tasks CSV script.
+
+2. Run it with upython. Give it the `-h`, `--help`, or `help` argument for help.
+```sh
+upython sharedTasksCSV.upy -h
+```
+
+3. To have a dynamic file name, you can add {arch}, {date}, or {time} (case-insensitive) to the value of the `-csvFileName` argument.
+
+# Setup: Create an Architecture
+
+## Automatic Architecture Creation
+
+**Tasks & Priorities**
+
+1. [Download](https://raw.githubusercontent.com/stinb/plugins/main/Solutions/sharedTasks/archBuilderForSharedTasks.py) the Architecture Builder for Shared Tasks script.
+
+2. Run it with upython.
+```sh
+upython archBuilderForSharedTasks.py
+```
+
+3. Follow the prompts to add functions as tasks.
+
+**Interrupt Control Functions (Optional):**
+
+1. Follow the prompts to add functions as interrupt control.
+
+## Manual Architecture Creation
+
+**Tasks & Priorities:**
+
+1. Create a new architecture in Understand.
+
+2. In this architecture, create a group with "task" or "priority" in the name (capitalization doesn't matter whenever a specific group name is needed.)
+
+3. In this Task Priority group, add a function. Optionally, you can put these functions in an integer group, which indicates the priority of the task.
+
+**Interrupt Control Functions (Optional):**
+
+1. In the architecture, create a group with "interrupt" or "control" in the name.
+
+2. In the Interrupt Control group, make a group with any name. It will be the ancestor of a pair of interrupt control functions.
+
+3. In the group with any name, add 2 groups: one with "disable" in the name and another with "enable" in the name.
+
+4. Finally, add 1 function to the Disable group and a different function to the Enable group.
+
+# Usage
+
+Once an architecture is created, right click it to visualize the shared tasks.
---
+++ Solutions/sharedTasks/archBuilderForSharedTasks.py
@@ -0,0 +1,135 @@
+# CLI to create architectures for the Shared Tasks graph
+
+
+import understand
+import sys
+
+
+def error(string):
+    print(f'\033[91m{string}\033[0m')
+
+
+def getNum(string, minimum=None, maximum=None):
+    while True:
+        num = input(string)
+        if num:
+            try:
+                num = int(num)
+                if minimum != None and num < minimum:
+                    raise IndexError(f'Minimum {minimum}')
+                if maximum != None and num > maximum:
+                    raise IndexError(f'Maximum {maximum}')
+            except:
+                error('Not a valid number')
+                continue
+        else:
+            num = None
+        break
+    return num
+
+
+def getFunction(functions, functionType):
+    while True:
+        name = input(f'{functionType} function name: ')
+        # Try again if function found
+        if name not in functions:
+            error('Not found')
+            continue
+        # If not ambiguous, number is 0
+        if len(functions[name]) == 1:
+            n = 0
+        # If ambiguous, list duplicate functions and get number
+        else:
+            print('Duplicates:')
+            for i in range(len(functions[name])):
+                print(f'({i}) {functions[name][i].uniquename()}')
+            n = num('Function number: ', 0, len(functions[name]) - 1)
+        return functions[name][n]
+
+
+def getChar(prompt, chars):
+    while True:
+        char = input(prompt)
+        if char:
+            char = char[0].lower()
+            if char in chars:
+                return char
+
+
+def main():
+    print('Quit anytime with "control c"')
+
+    # Get user input for database
+    db = None
+    if len(sys.argv) >= 2:
+        db = sys.argv[1]
+    while True:
+        db = db or input('\nDatabase path: ')
+        try:
+            db = understand.open(db)
+        except:
+            error('Error opening database')
+            error(e)
+            db = None
+            continue
+        break
+
+    # Make a dictionary { name: [ ent, ... ], ... }
+    functions = dict()
+    for ent in db.ents('Function'):
+        name = ent.name()
+        if name not in functions:
+            functions[name] = []
+        functions[name].append(ent)
+
+    # Get user input for architecture name
+    if len(sys.argv) >= 3:
+        arch = sys.argv[2]
+    else:
+        while True:
+            print()
+
+            # Architecture
+            arch = input('Architecture longname: ')
+            if not arch:
+                error('No name')
+                continue
+            if arch == 'Directory Structure':
+                error('Invalid name')
+                continue
+            break
+
+    # Get user input for each function
+    while True:
+        print()
+
+        # Task or interrupt control
+        char = getChar('(T)ask or (I)nterrupt Control: ', {'t', 'i'})
+
+        # Task
+        if char == 't':
+            # Group: task priority
+            group = getNum('Task priority: ')
+
+            # Function: task
+            task = getFunction(functions, 'Task')
+            if group:
+                task.add_to_arch(f'{arch}/Task Priority/{group}')
+            else:
+                task.add_to_arch(f'{arch}/Tasks')
+
+        # Interrupt control
+        else:
+            # Group: Interrupt control
+            group = input('Interrupt control name: ')
+
+            # Function: disable
+            disable = getFunction(functions, 'Disable')
+            disable.add_to_arch(f'{arch}/Interrupt Control/{group}/Disable')
+
+            # Function: enable
+            enable = getFunction(functions, 'Enable')
+            enable.add_to_arch(f'{arch}/Interrupt Control/{group}/Enable')
+
+
+main()
