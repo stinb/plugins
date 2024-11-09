2cd77c629c830eb76910551d879a2a98b39d9df9
Kenneth Catadman <kenneth@scitools.com>
2024-11-09 17:42:13 +0800

    Add check MISRA23_11.3.1: Variables of array type should not be declared

---
+++ CodeCheck/Published Standards/MISRA C++ 2023/MISRA23_11.3.1/test.cpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include <string>
+
+void foo() noexcept
+{
+    const size_t size{10};
+    wchar_t a1[size];             // UndCC_Violation
+    std::array<wchar_t, size> a2; // UndCC_Valid
+}
+
+void bar(int a[10],    // Rule does not apply - pointer to int
+         int (&b)[10], // Rule does not apply - reference to array
+         int (*c)[10]) // Rule does not apply - pointer to array
+{
+}
+
+struct S
+{
+    std::uint16_t a3[10]; // UndCC_Violation
+};
+
+using namespace std::literals;
+const char s1[] = "abcd";   // UndCC_Valid by exception
+char s2[] = "abcd";         // UndCC_Violation
+const auto best = "abcd"sv; // UndCC_Valid
