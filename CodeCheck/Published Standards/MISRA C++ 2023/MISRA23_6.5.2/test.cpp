4543c298f2c6038b09750e87d4504f36f9e29109
Kenneth Catadman <kenneth@scitools.com>
2024-11-12 16:04:21 +0800

    Add check MISRA23_6.5.2: Internal linkage should be specified appropriately

---
+++ CodeCheck/Published Standards/MISRA C++ 2023/MISRA23_6.5.2/test.cpp
@@ -0,0 +1,10 @@
+#include <cstdint>
+
+void f1(); // UndCC_Violation
+
+namespace
+{
+    void f2();                 // UndCC_Valid
+    int32_t notExtern1;        // UndCC_Valid
+    extern int32_t notExtern2; // UndCC_Violation
+}
