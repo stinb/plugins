b656fecb5efc382c9bf9132b175b11b313393431
Robby Bennett <robby@scitools.com>
2024-11-12 11:32:52 -0700

    internal: fix test file for CPP_N011

--- CodeCheck/All Checks/Language Specific/C and C++/Naming Conventions/CPP_N011/length.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Naming Conventions/CPP_N011/length.cpp
@@ -1,5 +1,5 @@
 // Length of 2-3
-int a;    // UndCC_Violation
+int a;    // UndCC_Violation(caps_all,caps_upper,length,prefix_suffix_required,regexp)
 int aa;   // UndCC_Violation(caps_all,caps_upper,prefix_suffix_required,regexp)
 int aaa;  // UndCC_Violation(caps_all,caps_upper,prefix_suffix_required,regexp)
-int aaaa; // UndCC_Violation
+int aaaa; // UndCC_Violation(caps_all,caps_upper,length,prefix_suffix_required,regexp)
