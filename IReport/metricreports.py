cfeccddc9572b78740f8792a670baa5ec51e5161
Natasha Stander <natasha@scitools.com>
2024-12-16 13:25:06 -0700

    Add Halstead Metrics interactive report
    Also adds progress and abort support to line and complexity metric reports and fixes typo in halstead vocabulary metric description.

--- IReport/metricreports.py
+++ IReport/metricreports.py
@@ -1,13 +1,15 @@
 import understand
 
-def metricsTable(report, metriclist, targetlist, link=False, filenameformat="long"):
+def metricsTable(report, metriclist, targetlist, link=False, filenameformat="long", names=dict(), prog_min = 0, prog_max=100):
   tbldesc = """ [{ "name" : "Name", "filtertype": "string", "sort" : "ascending" } """
   for metric in metriclist:
-    tbldesc += ",{ \"name\": \"" + metric + "\", \"filtertype\": \"numeric\"}"
+    tbldesc += ",{ \"name\": \"" + names.get(metric,metric) + "\", \"filtertype\": \"numeric\"}"
   tbldesc += "]"
   report.table(tbldesc)
 
-  for target in targetlist:
+  prog_range = len(targetlist)
+  last_prog = prog_min
+  for i, target in enumerate(targetlist):
     report.tablecell()
     if isinstance(target, understand.Arch):
       report.pagelink(target.longname())
@@ -31,4 +33,29 @@ def metricsTable(report, metriclist, targetlist, link=False, filenameformat="lon
       val = target.metric(m)
       if val is not None:
         report.print(str(val))
+    if prog_min is not None:
+      cur_prog = int(prog_min + (i / prog_range * (prog_max-prog_min))) # percentage
+      if cur_prog > last_prog:
+        report.progress(cur_prog)
+        last_prog = cur_prog
+    if report.is_aborted():
+      return
+  report.table()
+
+def targetMetricsTable(report, target, metrics, names=dict()):
+  report.table("""
+    [{
+      "name" : "Metric",
+      "filtertype": "string",
+      "sort" : "ascending"
+     },{
+      "name" : "Value",
+      "filtertype": "numeric"
+    }]""")
+  for k,v in target.metric(metrics).items():
+    report.tablecell()
+    report.print(names.get(k, k))
+
+    report.tablecell()
+    report.print(str(v))
   report.table()
