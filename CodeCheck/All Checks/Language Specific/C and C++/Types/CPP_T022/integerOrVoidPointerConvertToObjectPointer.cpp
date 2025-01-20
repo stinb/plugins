193ffc3fa33e009c5c3d1634584d59d73e29651e
jasonquinn <quinn@scitools.com>
2025-01-20 14:31:43 -0700

    Fixed: discrepancies between slight differences between standards in the same duplicate check script including MISRA12_11.5, M5-2-8, MISRA08_5-2-8, MISRA23_11.5, MISRA23_8.2.6, and CPP_T022 #34

--- CodeCheck/All Checks/Language Specific/C and C++/Types/CPP_T022/integerOrVoidPointerConvertToObjectPointer.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Types/CPP_T022/integerOrVoidPointerConvertToObjectPointer.cpp
@@ -2,7 +2,9 @@
 
 typedef int32_t myInt;
 typedef void myVoid;
-typedef myInt osbcureInt;
+typedef myInt obscureInt;
+
+enum class Color { Red, Green, Blue };
 
 struct S
 {
@@ -10,35 +12,42 @@ struct S
   int32_t j;
 };
 
-void f ( void * v, int32_t i )
+void f(void* v, int32_t i)
 {
-  char * v2;
-  S * s1 = reinterpret_cast< S * >( v ); // UndCC_Violation
-  S * s2 = reinterpret_cast< S * >( i ); // UndCC_Violation
-  S * s3;
-  
-  s3 = reinterpret_cast< S * >( v2 ); // UndCC_Valid
+  char* v2;
+  S* s1 = reinterpret_cast<S*>(v); // UndCC_Violation(0,1,2)
+  S* s2 = reinterpret_cast<S*>(i); // UndCC_Violation(1,2)
+  S* s3;
+
+  s3 = reinterpret_cast<S*>(v2); // UndCC_Valid
 }
 
-void f2 ( void * p1, int32_t i )
+void f2(void* p1, int32_t i)
 {
- S * s1 = static_cast< S * >( p1 ); // UndCC_Violation
- S * s2 = reinterpret_cast< S * >( i ); // UndCC_Violation
- void * p2 = reinterpret_cast< void * >( i ); // UndCC_Violation
- auto p3 = const_cast< void const * >( p2 ); // UndCC_Valid
+  S* s1 = static_cast<S*>(p1); // UndCC_Violation(0,1,2)
+  S* s2 = reinterpret_cast<S*>(i); // UndCC_Violation(1,2)
+  void* p2 = reinterpret_cast<void*>(i); // UndCC_Violation(1,2)
+  auto p3 = const_cast<void const*>(p2); // UndCC_Valid
 }
 
-
-void g ( myVoid * v, myInt i )
+void g(myVoid* v, myInt i)
 {
-  char * v2;
-  S * s2 = reinterpret_cast< S * >( i ); // UndCC_Violation
-  S * s1 = reinterpret_cast< S * >( v ); // UndCC_Violation
+  char* v2;
+  S* s2 = reinterpret_cast<S*>(i); // UndCC_Violation(1,2)
+  S* s1 = reinterpret_cast<S*>(v); // UndCC_Violation(0,1,2)
 }
 
-void h ( void * v, osbcureInt i )
+void h(void* v, obscureInt i)
 {
-  S * s1 = reinterpret_cast< S * >( v ); // UndCC_Violation
-  S * s2 = reinterpret_cast< S * >( i ); // UndCC_Violation
+  S* s1 = reinterpret_cast<S*>(v); // UndCC_Violation(0,1,2)
+  S* s2 = reinterpret_cast<S*>(i); // UndCC_Violation(1,2)
+}
+
+void enumToPointerExample(void* v)
+{
+  Color color = Color::Red;
+  void* ptr = reinterpret_cast<void*>(color); // UndCC_Violation(2)
+  S* sPtr = reinterpret_cast<S*>(color); // UndCC_Violation(2)
+
 }
 
