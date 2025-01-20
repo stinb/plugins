ffc89b533037f49cfb88c2dbd2f3c429f2b1c5b1
Kyle <kyle@scitools.com>
2025-01-20 10:40:07 -0700

    Internal: Add missing test file for MISRA 0-1-7

---
+++ CodeCheck/Published Standards/MISRA-C++ 2008/MISRA08_0-1-7/test.cpp
@@ -0,0 +1,12 @@
+#include <cstdint>
+
+uint16_t func ( uint16_t para1 )
+{
+    return para1;
+}
+
+void discarded ( uint16_t para2 )
+{
+    func ( para2 ); // UndCC_Violation
+    (void)func ( para2 ); // UndCC_Valid
+}
