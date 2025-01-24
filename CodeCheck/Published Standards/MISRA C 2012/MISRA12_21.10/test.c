9eebd6c23b34c2fa7e177823289dfe1ef60bbc0f
Kenneth Catadman <kenneth@scitools.com>
2025-01-24 16:49:57 +0800

    Add check MISRA23_21.10: The Standard Library time and date functions shall not be used

--- CodeCheck/Published Standards/MISRA C 2012/MISRA12_21.10/test.c
+++ CodeCheck/Published Standards/MISRA C 2012/MISRA12_21.10/test.c
@@ -1,1 +1,20 @@
+#include <wchar.h>
+#include <locale.h>
 #include <time.h> // UndCC_Violation
+
+int main()
+{
+    // Set locale for wide-character support
+    setlocale(LC_ALL, "");
+
+    // Get current time
+    time_t now = time(NULL);
+    struct tm *timeinfo = localtime(&now);
+
+    // Format and print the time
+    wchar_t buffer[100];
+    wcsftime(buffer, 100, L"%Y-%m-%d %H:%M:%S", timeinfo); // UndCC_Violation(wcsftime)
+    wprintf(L"Current time: %ls\n", buffer);
+
+    return 0;
+}
