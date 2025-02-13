a4ce1de0bfcbf1c004aa7d70eb8551ee6b647caf
jasonquinn <quinn@scitools.com>
2025-02-13 15:49:58 -0700

    Internal: fixed analysis errors in CPP_E024

--- CodeCheck/All Checks/Language Specific/C and C++/Expressions/CPP_E024/signedOperand.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Expressions/CPP_E024/signedOperand.cpp
@@ -1,16 +1,17 @@
 
 signed long okay = 1;
 signed long okay2 = 2;
-signed float violation1 = 3;
+float violation1 = 3;
 signed long answer;
-signed double violation2;
+double violation2;
 
-void main() {
-answer = violation2/okay2; // UndCC_Violation
-answer = okay2/violation1; // UndCC_Violation
-answer = okay%violation2; // UndCC_Violation
-answer = violation1/okay; // UndCC_Violation
-answer = okay%violation2; // UndCC_Violation
+int main() {
+  answer = violation2/okay2; // UndCC_Violation
+  answer = okay2/violation1; // UndCC_Violation
+  //answer = okay%violation2; // Violation and analysis error
+  answer = violation1/okay; // UndCC_Violation
+  //answer = okay%violation2; // Violation and analysis error
 
-return 0;
+  return 0;
 }
+
