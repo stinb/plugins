90d5da981204e85118a60afcc976c9b99646af66
jasonquinn <quinn@scitools.com>
2025-02-17 15:38:40 -0700

    Internal: fixed analysis errors in test files for CPP_E013

--- CodeCheck/All Checks/Language Specific/C and C++/Exception Handling/CPP_E013/test2.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Exception Handling/CPP_E013/test2.cpp
@@ -2,6 +2,17 @@
 #include <stdexcept>
 using std::logic_error;
 
+class class1 {
+public:
+  class1(const char* msg) : msg_(msg) {}
+  const char* what() const { return msg_; }
+
+private:
+  const char* msg_;
+};
+
+class1 ret_class1(char *);
+
 void func1(int a) {
   if (a)
     ;
@@ -9,14 +20,13 @@ void func1(int a) {
     throw logic_error("error"); // possible violation, unknown return type // UndCC_Violation
 }
 
-class class1 ret_class1(char *);
 void func2(int a) {
   throw ret_class1("error");	// okay, class return type
 }
 
 int ret_int1(char *);
+int ret_int2(char *);
 void func3(int a ) {
   throw ret_int1("error");	// UndCC_Violation, int return type
   throw ret_int2("error");	// possible violation, unknown return type // UndCC_Violation
 }
-
