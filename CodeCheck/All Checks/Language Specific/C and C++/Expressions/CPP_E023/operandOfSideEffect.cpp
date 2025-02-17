26e884c426944dce8fd4c11fd4c28f0c2f5fd7f6
jasonquinn <quinn@scitools.com>
2025-02-17 13:00:33 -0700

    Internal: fixed analysis errors in test files for CPP_E023

--- CodeCheck/All Checks/Language Specific/C and C++/Expressions/CPP_E023/operandOfSideEffect.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Expressions/CPP_E023/operandOfSideEffect.cpp
@@ -1,15 +1,14 @@
 // okay
 
 void okay1() { for (int obj=0; obj<10; ++obj) obj++; };
-void okay2() { int obj; do obj while (++obj); }
+void okay2() { int obj; do obj; while (++obj); }
 void okay3() { int obj; while (++obj) obj++; }
 void okay4() { int obj; if (obj++) obj++; }
 void okay5() { int obj; switch (obj++) { case 0: obj++; break; } }
-
+void func(int a, int b);
 
 
 void violation1() { int obj; obj = obj++; } // UndCC_Violation
-void violation2() { int obj; obj++ = obj; } // UndCC_Violation
 void violation3() { int obj; obj == ++obj; } // UndCC_Violation
 void violation4() { int obj; func(obj,++obj); } // UndCC_Violation
 void violation5() { int array[10],obj; array[obj++] = obj; } // UndCC_Violation
@@ -20,3 +19,5 @@ void violation6() { int array[10],obj; array[obj++] = array[obj++]; } // UndCC_V
 void exception1() { int obj; obj = ++obj; } // UndCC_Violation
 void exception2() { int obj; obj && ++obj; } // UndCC_Violation
 void exception3() { int obj; obj,++obj; } // UndCC_Violation
+
+
