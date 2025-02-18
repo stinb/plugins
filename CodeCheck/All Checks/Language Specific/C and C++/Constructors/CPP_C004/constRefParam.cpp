46ae88af1394515298591f7a91cb224c915b074a
jasonquinn <quinn@scitools.com>
2025-02-18 15:12:37 -0700

    Internal: fixed analysis errors in test files for CPP_C004

--- CodeCheck/All Checks/Language Specific/C and C++/Constructors/CPP_C004/constRefParam.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Constructors/CPP_C004/constRefParam.cpp
@@ -3,10 +3,10 @@
 
 class Okay1 {
   Okay1(const Okay1&) {};
-  Okay1(Okay1) {}; // not a copy constructor
 };
 
 
 class Violation1 {
   Violation1(Violation1& badAgain) {}; // UndCC_Violation
 };
+
