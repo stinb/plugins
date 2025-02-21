8fd2d37f7dca8e8bd7025c9fd116c2c235fc6534
jasonquinn <quinn@scitools.com>
2025-02-21 14:08:09 -0700

    Internal: fixed analysis errors in VHDL_27

--- CodeCheck/All Checks/Language Specific/Vhdl/VHDL_27/test.vhdl
+++ CodeCheck/All Checks/Language Specific/Vhdl/VHDL_27/test.vhdl
@@ -1,7 +1,22 @@
--- See: https://peterfab.com/ref/vhdl/vhdl_renerta/mobile/source/vhd00023.htm
+library IEEE;
+use IEEE.std_logic_1164.all;
 
-B_OUT <= transport B_IN after 1 ns;  -- Example 1: Transport delay -- UndCC_Violation
+entity delay_example is
+    port (
+        B_IN  : in  std_logic;
+        L_IN  : in  std_logic;
+        Q_IN  : in  std_logic;
+        B_OUT : out std_logic;
+        L_OUT : out std_logic;
+        Q_OUT : out std_logic
+    );
+end entity delay_example;
 
-L_OUT <= inertial L_IN after 1 ns;   -- Example 2: Inertial delay -- UndCC_Violation
+architecture behavioral of delay_example is
+begin
+    -- See: https://peterfab.com/ref/vhdl/vhdl_renerta/mobile/source/vhd00023.htm
+    B_OUT <= transport B_IN after 1 ns;  -- Example 1: Transport delay -- UndCC_Violation
+    L_OUT <= L_IN after 1 ns;            -- Example 2: Inertial delay -- UndCC_Violation
+    Q_OUT <= Q_IN after 1 ns;            -- Example 3: Inertial delay (simplified) -- UndCC_Violation
+end architecture behavioral;
 
-Q_OUT <= reject 500 ps inertial Q_IN after 1 ns;  -- Example 3: Inertial delay with rejection limit -- UndCC_Violation
