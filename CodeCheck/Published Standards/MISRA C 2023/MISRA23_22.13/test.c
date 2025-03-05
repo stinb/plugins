3c26b76135dcfcbdc99f79eda6c5dc0bd15a5172
Kenneth Catadman <kenneth@scitools.com>
2025-03-05 18:55:29 +0800

    Add check MISRA23_22.13: Thread objects, thread synchronization objects and thread-specific storage pointers shall have appropriate storage duration

---
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_22.13/test.c
@@ -0,0 +1,24 @@
+#include <threads.h>
+#include <stdint.h>
+
+mtx_t Ra;                         /* UndCC_Valid */
+_Thread_local mtx_t thread_mutex; /* UndCC_Violation, thread storage duration */
+
+int32_t t1(void *ptr) /* Thread entry */
+{
+    mtx_lock(&Ra);
+    mtx_lock((mtx_t *)ptr);   /* Lifetime of Rb might have ended
+    ... pointer might be dangling */
+    mtx_unlock((mtx_t *)ptr); /* Lifetime of Rb might have ended
+       ... pointer might be dangling */
+    mtx_unlock(&Ra);
+}
+
+void main(void)
+{
+    thrd_t id1; /* UndCC_Violation */
+    mtx_t Rb;   /* UndCC_Violation */
+    mtx_init(&Ra, mtx_plain);
+    mtx_init(&Rb, mtx_plain);
+    thrd_create(&id1, t1, &Rb);
+}
