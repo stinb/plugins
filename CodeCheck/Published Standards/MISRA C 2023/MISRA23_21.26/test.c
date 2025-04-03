b9f1fbd680f8929d8bc062123418cdfa93c94053
Kenneth Catadman <kenneth@scitools.com>
2025-04-03 20:10:14 +0800

    Add check MISRA23_21.26: The Standard Library function mtx_timedlock() shall only be invoked on mutex objects of appropriate mutex type

---
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_21.26/test.c
@@ -0,0 +1,22 @@
+#include <threads.h>
+#include <stdint.h>
+
+mtx_t Ra;
+mtx_t Rb;
+mtx_t Rc;
+
+struct timespec *ts;
+
+void main(void)
+{
+    mtx_init(&Ra, mtx_plain);
+    mtx_init(&Rb, mtx_timed);
+    mtx_init(&Rc, mtx_timed | mtx_recursive);
+}
+
+int32_t t1(void *ignore)
+{
+    mtx_timedlock(&Ra, ts); /* UndCC_Violation */
+    mtx_timedlock(&Rb, ts); /* UndCC_Valid */
+    mtx_timedlock(&Rc, ts); /* UndCC_Valid */
+}
