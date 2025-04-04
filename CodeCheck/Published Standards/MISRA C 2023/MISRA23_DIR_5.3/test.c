fa75b88ab77c96aadfa7340ae1d3b595ebe05c3c
Kenneth Catadman <kenneth@scitools.com>
2025-04-04 18:32:53 +0800

    Add check MISRA23_DIR_5.3: There shall be no dynamic thread creation

---
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_DIR_5.3/test.c
@@ -0,0 +1,22 @@
+#include <threads.h>
+#include <stdint.h>
+
+thrd_t id1;
+thrd_t id2;
+
+int32_t t2(void *ignore) /* Thread T2 entry */
+{
+    ///
+}
+
+int32_t t1(void *ignore) /* Thread T1 entry */
+{
+    thrd_create(&id2, t2, NULL); /* UndCC_Violation, not constrained to start-up */
+    ///
+}
+
+void main(void)
+{
+    thrd_create(&id1, t1, NULL); /* UndCC_Valid */
+    ///
+}
