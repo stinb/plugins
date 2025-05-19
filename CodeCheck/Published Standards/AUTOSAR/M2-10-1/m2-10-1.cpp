983e475af14bca2e2610fcdb1917658357f401fe
jordancolbeth <jordancolbeth@gmail.com>
2025-05-19 14:48:15 -0700

    internal: fix test file for M2-10-1

--- CodeCheck/Published Standards/AUTOSAR/M2-10-1/m2-10-1.cpp
+++ CodeCheck/Published Standards/AUTOSAR/M2-10-1/m2-10-1.cpp
@@ -1,18 +1,18 @@
 int id1_a_b_c;
 int id1_abc;  // UndCC_Violation
-int id2_abc;  // UndCC_Violation
-int id2_ABC;  
+int id2_abc;  
+int id2_ABC;  // UndCC_Violation
 int id3_a_bc;
 int id3_ab_c; // UndCC_Violation
 int id4_a_bc;
 int id4_ab_c; // UndCC_Violation
-int id5_ii; // UndCC_Violation
-int id5_11;   
-int id6_i0; // UndCC_Violation
-int id6_1O;   
-int id7_in; // UndCC_Violation
-int id7_1h;   
-int id8_Z5; // UndCC_Violation
-int id8_2S;   
-int id9_ZS; // UndCC_Violation
-int id9_25;   
+int id5_ii;
+int id5_11;  // UndCC_Violation 
+int id6_i0;
+int id6_1O;  // UndCC_Violation 
+int id7_in;
+int id7_1h;  // UndCC_Violation 
+int id8_Z5;
+int id8_2S;  // UndCC_Violation 
+int id9_ZS;
+int id9_25;  // UndCC_Violation 
