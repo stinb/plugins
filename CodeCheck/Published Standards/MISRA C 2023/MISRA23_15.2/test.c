291ebc3cbc4aa9e9478f75582cef63d2e886deac
Kenneth Catadman <kenneth@scitools.com>
2025-05-29 20:22:40 +0800

    Internal: Fix false positives in MISRA23_15.2 #958

---
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_15.2/test.c
@@ -0,0 +1,15 @@
+void f_152(void)
+{
+    int j_152 = 0;
+
+L1_152:
+    ++j_152;
+    if (10 == j_152)
+    {
+        goto L2_152; /* UndCC_Valid */
+    }
+    goto L1_152; /* UndCC_Violation */
+
+L2_152:
+    ++j_152;
+}
