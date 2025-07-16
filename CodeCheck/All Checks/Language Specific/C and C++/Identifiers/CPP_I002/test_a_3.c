4874728f056a0d80892bc5bd09aa5ec4fc43d511
jordancolbeth <jordancolbeth@gmail.com>
2025-07-16 13:55:07 -0700

    internal: fix uvalid option typos in CPP_I002

--- CodeCheck/All Checks/Language Specific/C and C++/Identifiers/CPP_I002/test_a_3.c
+++ CodeCheck/All Checks/Language Specific/C and C++/Identifiers/CPP_I002/test_a_3.c
@@ -4,6 +4,7 @@
 //  123456**********************************     Characters
 int a_aaa0;
 int a_aaa1;
+
 int a_bbbb0; // UndCC_Violation(a) - same as the next 1
 int a_bbbb1;
 
@@ -11,15 +12,16 @@ int a_bbbb1;
 //  1234567890123456789012345678901*********     Characters
 int a_cccccccccccccccccccccccccccc0;  // UndCC_Violation(a) - same as the next 1
 int a_cccccccccccccccccccccccccccc1;
-int a_ddddddddddddddddddddddddddddd0; // UndCC_Violation(a,c) - same as the next 1
+
+int a_ddddddddddddddddddddddddddddd0; // UndCC_Violation(a,b) - same as the next 1
 int a_ddddddddddddddddddddddddddddd1;
 
 // Types
 //          1234567890123456789012345678901*********     Characters
-typedef int a_eeeeeeeeeeeeeeeeeeeeeeeeeeeee0_t; // UndCC_Violation(a,c) - same as the next 1
+typedef int a_eeeeeeeeeeeeeeeeeeeeeeeeeeeee0_t; // UndCC_Violation(a,b) - same as the next 1
 typedef int a_eeeeeeeeeeeeeeeeeeeeeeeeeeeee1_t;
 
 // Functions
 //   1234567890123456789012345678901*********     Characters
-void a_fffffffffffffffffffffffffffff0_t() {} // UndCC_Violation(a,c) - same as the next 1
+void a_fffffffffffffffffffffffffffff0_t() {} // UndCC_Violation(a,b) - same as the next 1
 void a_fffffffffffffffffffffffffffff1_t() {}
