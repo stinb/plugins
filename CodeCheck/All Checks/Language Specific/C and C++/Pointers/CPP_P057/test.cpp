67c7ef6581a16b5f6d9ebd59c6cabb5b12f4399c
Jason Quinn <quinn@scitools.com>
2025-09-09 10:05:52 -0600

    Internal: fixed analyisis errors for windows and mac in CPP_P057

--- CodeCheck/All Checks/Language Specific/C and C++/Pointers/CPP_P057/test.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Pointers/CPP_P057/test.cpp
@@ -4,6 +4,5 @@ void test() {
   
   int *ptr1 = new int[10]; // UndCC_Violation
   
-  // UndCC_Valid
-  std::auto_ptr<int> ptr2(new int[10]);
+  std::unique_ptr<int> ptr2(new int[10]); // UndCC_Valid
 }
