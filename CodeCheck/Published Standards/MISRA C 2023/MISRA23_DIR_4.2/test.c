d1301818bfcd43266fd6ed8964d706aa36e5aa79
Jason Quinn <quinn@scitools.com>
2025-09-10 10:01:23 -0600

    Internal: fixed script errors on mac for MISRA23_DIR_4.2

--- CodeCheck/Published Standards/MISRA C 2023/MISRA23_DIR_4.2/test.c
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_DIR_4.2/test.c
@@ -3,6 +3,7 @@
 int main() {
   int a = 5, b = 10, result;
 
+#if defined(__linux__) || defined(__APPLE__)
   // Inline assembly to add a and b UndCC_Valid
   __asm__ (
     "movl %1, %%eax;"    // Move a into eax
@@ -10,7 +11,7 @@ int main() {
     "movl %%eax, %0;"    // Store result in result
     : "=r" (result)      // Output operand
     : "r" (a), "r" (b)   // Input operands
-    : "%rax"             // Clobbered register
+    : "%eax"             // Clobbered register
   );
 
   __asm__ ( // UndCC_Violation
@@ -19,8 +20,9 @@ int main() {
     "movl %%eax, %0;"   
     : "=r" (result)      
     : "r" (a), "r" (b)   
-    : "%rax"             
+    : "%eax"             
   );
+#endif
   printf("Sum of %d and %d is %d\n", a, b, result);
   return 0;
 }
