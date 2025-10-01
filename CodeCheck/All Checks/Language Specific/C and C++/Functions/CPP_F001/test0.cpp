fa5963b4dab27ffc3218bdf30ba4bb93c9f03696
Robby Bennett <robby@scitools.com>
2025-10-01 16:05:02 -0600

    internal: combine MISRA08_14-7-1, CPP_F001

--- CodeCheck/All Checks/Language Specific/C and C++/Functions/CPP_F001/test0.cpp
+++ CodeCheck/All Checks/Language Specific/C and C++/Functions/CPP_F001/test0.cpp
@@ -1,19 +1,21 @@
+// MISRA C++ 2008
+
 template < typename T > // UndCC_Violation
-class Sample1471
+class Sample
 {
 public:
-  void inst_mem1471 ( )
+  void inst_mem ( )
   {
     // ...
   }
-  void uninst_mem1471 ( )  // UndCC_Violation
+  void uninst_mem ( )  // UndCC_Violation
   {
     // ...
   }
 };
 int main () {
-  Sample1471<long int> s1471;
-  s1471.inst_mem1471();      // Call to s.inst_mem instantiates the member.
+  Sample<long int> s;
+  s.inst_mem();      // Call to s.inst_mem instantiates the member.
                              // s.uninst_mem is not called within the program
                              // and is not instantiated.
   return 0;
