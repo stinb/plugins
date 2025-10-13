cb64875a977f4b2586eac129853761b77a5f0e93
Robby Bennett <robby@scitools.com>
2025-10-13 11:27:27 -0600

    internal: combine MISRA12_5.8, MISRA23_5.8, CPP_I003

--- CodeCheck/Published Standards/MISRA C 2012/MISRA12_5.8/5.8externalIdentifiersUniqueA.c
+++
@@ -1,7 +0,0 @@
-/* 5.8externalIdentifiersUniqueA.c */
-
-int count_58; 			/* "count" has external linkage */
-void foo_58 ( void ) 	/* "foo" has external linkage */
-{
-	short index_58; 	/* "index" has no linkage */
-}
