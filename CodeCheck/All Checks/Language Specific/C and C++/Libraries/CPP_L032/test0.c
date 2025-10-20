bc7f429a2e981c5a310852d8b98e6ef1c1c57262
Robby Bennett <robby@scitools.com>
2025-10-20 11:56:29 -0600

    Add check MISRA23_1.4, CPP_L032: Bounds-checking Interfaces

---
+++ CodeCheck/All Checks/Language Specific/C and C++/Libraries/CPP_L032/test0.c
@@ -0,0 +1,13 @@
+// Custom
+
+// This could be defined in the CLI, so flag `strnlen_s` and others
+#define __STDC_WANT_LIB_EXT1__ 1
+
+#include <string.h>
+
+void f()
+{
+  const char s[] = "Hello";
+  strnlen_s(s, sizeof(s)); // UndCC_Violation
+  rsize_t x;               // UndCC_Violation
+}
