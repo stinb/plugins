8d2ed2fb2ac3efa4c4af76f91bbd63ccbb0eec67
Kenneth Catadman <kenneth@scitools.com>
2025-11-10 22:48:34 +0800

    Add check MISRA23_22.10: The value of errno shall only be tested when the last function to be called was an errno-setting-function

---
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_22.10/test.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <errno.h>
+
+typedef double float64_t;
+
+void f(void)
+{
+    float64_t f64;
+    errno = 0;
+    f64 = atof("A.12");
+    if (0 == errno) /* UndCC_Violation */
+    {
+        /* f64 may not have a valid value in here. */
+    }
+    errno = 0;
+    f64 = strtod("A.12", NULL);
+    if (0 == errno) /* UndCC_Valid */
+    {
+        /* f64 will have a valid value in here. */
+    }
+}
