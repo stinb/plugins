63494ccc6e4dc19197779f0c692df7c4fe20ab70
Kenneth Catadman <kenneth@scitools.com>
2025-11-10 17:58:31 +0800

    Internal: Fix false positive in MISRA23_0.2.4 #1061

--- CodeCheck/SciTools' Recommended Checks/RECOMMENDED_13/header.h
+++ CodeCheck/SciTools' Recommended Checks/RECOMMENDED_13/header.h
@@ -1,6 +1,6 @@
 int foo(); /* ignored, undefined prototype */
 int valid();
-int violation (); // UndCC_Violation
+int violation (); // UndCC_Violation(0)
 int main();
 class someclass{
   ~someclass();
