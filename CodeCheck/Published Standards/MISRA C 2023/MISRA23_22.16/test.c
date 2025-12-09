5b22c8c3d946c215f6a367881f27608b6066d99a
Jordan Colbeth <jordancolbeth@gmail.com>
2025-12-09 09:09:30 -0800

    internal: fix test file for MISRA23_22.16

--- CodeCheck/Published Standards/MISRA C 2023/MISRA23_22.16/test.c
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_22.16/test.c
@@ -1,4 +1,4 @@
-#include <threads.h>
+#include <threads.h> // C11 threads not supported by Mac/Linux
 #include <stdint.h>
 
 typedef int bool_t;
@@ -13,7 +13,7 @@ int32_t t1(void *ignore) /* Thread 1 */
     mtx_lock(&Ra); /* UndCC_Valid */
     mtx_unlock(&Ra);
 
-    mtx_lock(&Rb); /* UndCC_Violation - unlock missing on one path */
+    mtx_lock(&Rb); /* UndCC_Violation(Win) - unlock missing on one path */
 
     if (b)
     {
