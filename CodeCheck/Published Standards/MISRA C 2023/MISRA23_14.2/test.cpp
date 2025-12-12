59368161e8ff7d7d09af23ba8b15270ef26d1678
Jason Quinn <quinn@scitools.com>
2025-12-12 13:33:14 -0700

    Fixed: False Positives in MISRA23_14.2

--- CodeCheck/Published Standards/MISRA C 2023/MISRA23_14.2/test.cpp
+++ CodeCheck/Published Standards/MISRA C 2023/MISRA23_14.2/test.cpp
@@ -1,5 +1,10 @@
 #define UINT_32 unsigned int
+#include <stdint.h>
 int get_status();
+
+uint8_t g_c[10] = {1, 5, 3, 9, 2, 8, 4, 6, 7, 0};
+uint8_t g_array[10][10];
+
 int main() {
   UINT_32 uLen = 123456789;
   UINT_32 m_count[2] = {0};
@@ -26,7 +31,34 @@ int main() {
   for (int i = 0; i < 10; i++, flag = get_status()) { } /* UndCC_Violation: function call with side effects in iteration clause */
   for (int i = 0; i < 10; sum += i, i++) { } /* UndCC_Violation: iteration clause modifies sum (unrelated to loop control) */
   for (;;) {} // UndCC_Valid
+
+  /* UndCC_Valid: k is not the loop counter, modifying it in the body is allowed */
+  uint8_t k = 0;
+  for (uint8_t i = 1; i < 8; i++) { // UndCC_Valid
+    if (g_c[i] > g_c[k]) {
+      k = i;
+    }
+  }
 }
+
+void rgBusWr(unsigned int addr, unsigned char val, int param) {
+  // Dummy function
+}
+
+void g(void) {
+  unsigned char i;
+  for (i = 0; i < 8; i++) { // UndCC_Valid - i is only read in the body, not modified
+    rgBusWr(0x10a6 + i, 0x55, 1);
+  }
+}
+
+void h(void) {
+  uint8_t cycle = 10;
+  for (uint8_t i = 0; i < cycle; i++) { // UndCC_Valid - modifying array element, not loop counter i
+    g_array[i][5] = 42;
+  }
+}
+
 int get_status() {
   return 1;
 }
