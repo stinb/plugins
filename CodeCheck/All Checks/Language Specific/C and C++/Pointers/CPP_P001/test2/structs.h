e2350e5168ac024e7fad217a97b44f251eed6a59
Robby Bennett <22121365+RobbyCBennett@users.noreply.github.com>
2026-01-05 14:23:46 -0700

    Checks: Fix bug with non-pointers being flagged in MISRA12_DIR_4.8, MISRA23_DIR_4.8, CPP_P001 #4494

---
+++ CodeCheck/All Checks/Language Specific/C and C++/Pointers/CPP_P001/test2/structs.h
@@ -0,0 +1,14 @@
+// Custom
+
+struct Vec2
+{
+  float x;
+  float y;
+};
+
+typedef struct
+{
+  float x;
+  float y;
+  float z;
+} Vec3;
---
+++ CodeCheck/All Checks/Language Specific/C and C++/Pointers/CPP_P001/test2/usage.c
@@ -0,0 +1,18 @@
+// Custom
+
+#include "structs.h"
+
+struct Triangle
+{
+  struct Vec2 v1;
+  struct Vec2 v2;
+  struct Vec2 v3;
+};
+
+typedef struct
+{
+  Vec3 v1;
+  Vec3 v2;
+  Vec3 v3;
+  Vec3 v4;
+} Tetrahedron;
