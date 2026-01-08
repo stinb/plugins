46f0a0916627441debd32a33a0c64556c393aeed
Robby Bennett <22121365+RobbyCBennett@users.noreply.github.com>
2026-01-08 14:45:53 -0700

    Checks: Fix for multithreading in HIS_09, AC_HIS_09 #4517

---
+++ CodeCheck/Published Standards/Hersteller Initiative Software (HIS) Metrics/HIS_09/test0.c
@@ -0,0 +1,30 @@
+// Custom
+
+void validDepth1()
+{
+}
+
+void validDepth2()
+{
+  validDepth1();
+}
+
+void validDepth3()
+{
+  validDepth2();
+}
+
+void validDepth4()
+{
+  validDepth3();
+}
+
+void invalidDepth1() // UndCC_Violation
+{
+  validDepth4();
+}
+
+void invalidDepth2() // UndCC_Violation
+{
+  invalidDepth2();
+}
