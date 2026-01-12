2c98f6c90ad10d188343e06a381f69c2d44cad3f
Robby Bennett <22121365+RobbyCBennett@users.noreply.github.com>
2026-01-12 15:33:25 -0700

    Checks: Fix parameter count in HIS_07, METRIC_06, AC_HIS_07 #4517

--- CodeCheck/Published Standards/Hersteller Initiative Software (HIS) Metrics/HIS_07/07Parameters.c
+++
@@ -1,3 +0,0 @@
-int tooManyParameters(int a, int b, int c, int d, int e, int f) { // UndCC_Violation
-return 0;
-}
---
+++ CodeCheck/Published Standards/Hersteller Initiative Software (HIS) Metrics/HIS_07/test0.c
@@ -0,0 +1,11 @@
+// Custom
+
+int tooManyParameters(int a, int b, int c, int d, int e, int f) // UndCC_Violation
+{
+  return 0;
+}
+
+int onlyFive(int a, int b, int c, int d, int e) // UndCC_Valid
+{
+  return 0;
+}
