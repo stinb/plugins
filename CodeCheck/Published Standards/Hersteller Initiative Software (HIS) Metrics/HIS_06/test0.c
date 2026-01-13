46fce5c031693f2fdec989c79f4030ec799f74f2
Robby Bennett <22121365+RobbyCBennett@users.noreply.github.com>
2026-01-13 13:42:57 -0700

    internal: rewrite as python HIS_05, METRIC_01, AC_HIS_05, also move HIS_06 #4517

---
+++ CodeCheck/Published Standards/Hersteller Initiative Software (HIS) Metrics/HIS_06/test0.c
@@ -0,0 +1,37 @@
+// Custom
+
+void invalidCallers() // UndCC_Violation
+{
+  ;
+}
+
+void validCaller1()
+{
+  invalidCallers();
+}
+
+void validCaller2()
+{
+  invalidCallers();
+}
+
+void validCaller3()
+{
+  invalidCallers();
+}
+
+void validCaller4()
+{
+  invalidCallers();
+}
+
+void validCaller5()
+{
+  invalidCallers();
+}
+
+void validCaller6()
+{
+  validCaller1();
+  invalidCallers();
+}
