9a105885325426fadae8d29235225f2413a35017
Jason Quinn <quinn@scitools.com>
2026-01-16 11:32:11 -0700

    Checks: Fixed false positive in EXP46-C with logical operators #4547

--- CodeCheck/Published Standards/SEI Cert Standards/C/EXP46-C/test.c
+++ CodeCheck/Published Standards/SEI Cert Standards/C/EXP46-C/test.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #if __has_include(<unistd.h>)
 
 #include <unistd.h>
@@ -7,10 +9,32 @@ int main() {
     /* ... */
   }
 
-  // UndCC_Valid
-  if (!(getuid() && geteuid() == 0)) {
+  if (!(getuid() && geteuid() == 0)) { // UndCC_Valid
     /* ... */
   }
 }
 
 #endif // __has_include(<unistd.h>)
+
+// Test case from bug report
+void test_false_positive() {
+    uint8_t c_d = 0;
+    uint16_t g_sci = 0x1245;
+
+    if ( (c_d == 1) && ((g_sci & 0x0002U) != 0) ) { ; } // UndCC_Valid
+}
+
+// Missing parentheses
+void test_real_violation() {
+    uint8_t a = 5;
+    uint8_t b = 10;
+
+    if (a & b == 5) { ; }  // UndCC_Violation
+}
+
+// Properly parenthesized bitwise operation
+void test_compliant() {
+    uint16_t flags = 0x1234;
+
+    if ((flags & 0x0001U) != 0) { ; } // UndCC_Valid
+}
