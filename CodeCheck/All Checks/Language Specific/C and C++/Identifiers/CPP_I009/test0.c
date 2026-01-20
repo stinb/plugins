680b174b2a560ede83d305699c878b015161722c
Robby Bennett <22121365+RobbyCBennett@users.noreply.github.com>
2026-01-20 16:10:19 -0700

    internal: checks: rewrite and combine MISRA12_5.4, MISRA23_5.4, CPP_I009 #4517

---
+++ CodeCheck/All Checks/Language Specific/C and C++/Identifiers/CPP_I009/test0.c
@@ -0,0 +1,9 @@
+/* MISRA C 2012 & 2023 */
+
+/*      1234567890123456789012345678901*********               Characters */
+#define engine_exhaust_gas_temperature_raw     egt_r
+#define engine_exhaust_gas_temperature_scaled  egt_s   /* UndCC_Violation */
+
+/*      1234567890123456789012345678901*********               Characters */
+#define engine_exhaust_gas_temp_raw            egt_r
+#define engine_exhaust_gas_temp_scaled         egt_s   /* UndCC_Valid     */
---
+++ CodeCheck/All Checks/Language Specific/C and C++/Identifiers/CPP_I009/test1.c
@@ -0,0 +1,5 @@
+// Custom
+
+#define min(X, Y) ((X) < (Y) ? (X) : (Y)) //	UndCC_Valid
+
+#define max(engine_exhaust_gas_temperature_X, engine_exhaust_gas_temperature_Y)  ((engine_exhaust_gas_temperature_X) > (engine_exhaust_gas_temperature_Y) ? (engine_exhaust_gas_temperature_Y) : (engine_exhaust_gas_temperature_X)) // UndCC_Violation
