8c8c32aa5fe136f8f8ea48f7187e1b8b8f2a9c63
Jason Quinn <quinn@scitools.com>
2026-02-26 10:45:38 -0700

    Checks: Fixed: False negatives in MISRA12_17.3 for functions with no definition in project #4675

--- CodeCheck/SciTools' Recommended Checks/RECOMMENDED_05/bar.c
+++ CodeCheck/SciTools' Recommended Checks/RECOMMENDED_05/bar.c
@@ -3,6 +3,12 @@
 #include "foo.h"
 
 int bar() {
-    return foo();     // UndCC_Violation: foo is not declared 
+    return foo();     // UndCC_Violation: foo is not declared
     // This function call will now become an implicit declaration
+}
+
+// Function called with no declaration or definition in the project
+void func( void )
+{
+    double sq1 = power( 1, 2.0 ); // UndCC_Violation
 }
