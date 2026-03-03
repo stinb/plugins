2b7b0c1ce02de90e79f3850c404aa59e9ec5c40a
Jason Quinn <quinn@scitools.com>
2026-03-03 13:13:58 -0700

    Checks: Fixed: False negatives in MISRA23_14.4 with bitwise operators #4688

--- CodeCheck/Published Standards/MISRA C 2012/MISRA12_14.4/test.c
+++ CodeCheck/Published Standards/MISRA C 2012/MISRA12_14.4/test.c
@@ -15,7 +15,7 @@ while ( true ) /* UndCC_Valid */
 {
 }
 extern bool flag;
-while ( flag ) /* UnddCC_Valid */
+while ( flag ) /* UndCC_Valid */
 {
 }
 int32_t i;
@@ -24,6 +24,20 @@ if ( i ) /* UndCC_Violation */
 }
 if ( i != 0 ) /* UndCC_Valid */
 {
+}
+
+// Bitwise operators produce integer results, not boolean
+int j;
+if ( i & 0x0F ) { // UndCC_Violation
+    j = 1;
+} else {
+    j = 3;
+}
+if ( i | 0x0F ) { // UndCC_Violation
+    j = 1;
+}
+if ( (i & 0x0F) != 0 ) { // UndCC_Valid
+    j = 1;
 }
 }
 
