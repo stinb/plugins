c539d94f6e72d599851f44259bda973bb04a1c0a
Kenneth Catadman <kenneth@scitools.com>
2026-04-16 16:01:23 +0800

    Checks: Add check ADA95_4.3.1: Using Exceptions to Help Define an Abstraction

---
+++ CodeCheck/Published Standards/Ada 95/ADA95_4.3.1/test.ads
@@ -0,0 +1,7 @@
+package Test_4_3_1 is
+   procedure Raise_User_Defined;
+   procedure Raise_Constraint_Error;
+   procedure Raise_Program_Error;
+   procedure Raise_Storage_Error;
+   procedure Raise_Tasking_Error;
+end Test_4_3_1;
